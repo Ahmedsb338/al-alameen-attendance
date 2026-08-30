da2fd019f37e8a1de01eaf8ad38b1a114c1abab9
Ahmed Sobhy <ahmedsb338@gmail.com>
2026-08-31 02:44:13 +0300

    Clean up unused files for production

--- JS/config.js
+++
@@ -1,9 +0,0 @@
-// =====================================================
-// SUPABASE CONFIGURATION
-// =====================================================
-
-const SUPABASE_URL =
-    "https://lwbyjnzpmtlflhsywtcw.supabase.co";
-
-const SUPABASE_KEY =
-    "sb_publishable_BeMQaIT1VmSjOb9B4t8VdA_QQAkS-jF";
